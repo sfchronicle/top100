07b8511f8b623a12dc68e2fc1d40601565be3d4d
Lucio Villa <lucio310@gmail.com>
2017-01-11 15:45:37 -0800

    added 2017 contenders guide page

--- views.py
+++ views.py
@@ -122,6 +122,92 @@ def restaurant_view(slug):
 		restaurants=restaurants
 	)
 
+@app.route('/guide/2017-contenders/')
+def contenders():
+	restaurants = [
+  {
+    "restaurant": "Original Joe's Westlake",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Original-Joe-s-Westlake-brings-back-the-past-7296147.php",
+    "img": "9847007"
+  },
+  {
+    "restaurant": "Leo's Oyster Bar",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Michael-Bauer-Leo-s-Oyster-Bar-s-refined-7397699.php",
+    "img": "9937518"
+  },
+  {
+    "restaurant": "Aster",
+    "link": "http://www.sfchronicle.com/restaurants/article/Aster-now-a-shining-star-in-the-Mission-7945247.php",
+    "img": "12168935"
+  },
+  {
+    "restaurant": "Rintaro",
+    "link": "http://www.sfchronicle.com/restaurants/article/Rintaro-evolves-into-loving-expression-of-7955450.php",
+    "img": "10168331"
+  },
+  {
+    "restaurant": "Picco in Larkspur",
+    "link": "http://www.sfchronicle.com/restaurants/article/Alluring-Picco-doesn-t-miss-a-beat-with-new-chef-8319082.php",
+    "img": "12168943"
+  },
+  {
+    "restaurant": "Ju-ni",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Ju-ni-is-a-sushi-star-for-a-new-generation-8323351.php",
+    "img": "10410073"
+  },
+  {
+    "restaurant": "Miminishi in Napa",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Inspired-by-visit-to-Japan-chef-s-Miminashi-8348094.php",
+    "img": "10479970"
+  },
+  {
+    "restaurant": "Bellota",
+    "link": "http://www.sfchronicle.com/food/article/Bellota-is-the-Absinthe-Group-s-biggest-and-8403303.php",
+    "img": "10558365"
+  },
+  {
+    "restaurant": "Cockscomb",
+    "link": "http://www.sfchronicle.com/restaurants/article/Chris-Cosentino-s-Cockscomb-boldly-earns-three-7266082.php",
+    "img": "9835968"
+  },
+  {
+    "restaurant": "In Situ",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Corey-Lee-s-culinary-artistry-at-In-Situ-the-9173165.php",
+    "img": "10752105"
+  },
+  {
+    "restaurant": "Bluestem",
+    "link": "http://www.sfchronicle.com/restaurants/article/S-F-s-Bluestem-Brasserie-soars-to-three-stars-9208030.php",
+    "img": "12169053"
+  },
+  {
+    "restaurant": "Corridor",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Mid-Market-s-new-Corridor-reinvents-and-rewards-9213223.php",
+    "img": "10865492"
+  },
+  {
+    "restaurant": "Playa in Mill Valley",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Vamos-a-la-Playa-Inspired-Mexican-cuisine-in-9517222.php",
+    "img": "10935201"
+  },
+  {
+    "restaurant": "Seven Hills",
+    "link": "http://www.sfchronicle.com/restaurants/article/Neighborhood-restaurant-Seven-Hills-is-seeing-9967035.php",
+    "img": "11378379"
+  },
+  {
+    "restaurant": "Nightbird",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/Nightbird-flies-high-in-Hayes-Valley-10035557.php",
+    "img": "11487838"
+  },
+  {
+    "restaurant": "Saratoga",
+    "link": "http://www.sfchronicle.com/restaurants/diningout/article/The-Saratoga-Cuisine-designed-for-cocktails-in-10840351.php",
+    "img": "12123354"
+  }
+]
+	return render_template('2017_contenders.html', restaurants=restaurants)
+
 @freezer.register_generator
 def restaurant_view():
 	for restaurant in Restaurant.query.all():
